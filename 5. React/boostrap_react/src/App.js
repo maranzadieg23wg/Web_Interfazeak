d96f3a89070b74d59db7a3f97e8dfbb131f5eaec
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-11-24 13:32:00 +0100

    Menua eginda

--- 5. React/boostrap_react/src/App.js
+++ 5. React/boostrap_react/src/App.js
@@ -1,11 +1,15 @@
-import Header from './header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Header from './header';
+import Menu from './menu';
 
 
 function App() {
   return (
     <div className="App container">
       <Header />
+      <Menu />
+
     </div>
   );
 }
---
+++ 5. React/boostrap_react/src/menu.js
@@ -0,0 +1,38 @@
+
+function Menu() {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container">
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Quienes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Cursos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Situación</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+
+            
+        </div>
+    </nav>
+    );
+  }
+  
+  export default Menu;
+  
