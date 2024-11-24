36c19f9e8eaf42e574ff33b54e70056c233b2cb0
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-11-24 13:29:32 +0100

    Header sortuta

---
+++ 5. React/boostrap_react/src/header.js
@@ -0,0 +1,23 @@
+
+function Header() {
+  return (
+    <div className="row align-items-center">
+      <div className="col-sm-4">
+        <h1 className="text-center">ZUBIRI MANTEO</h1>
+      </div>
+
+      <div className="col-sm-4"></div>
+
+      <div className="col-sm-4">
+      <img
+        className="d-block rounded-circle shadow w-25 h-25"
+        src="./img/zubiri-manteo-logo.png"
+        alt="Zubiri Manteo Logo"
+        />
+
+      </div>
+    </div>
+  );
+}
+
+export default Header;
